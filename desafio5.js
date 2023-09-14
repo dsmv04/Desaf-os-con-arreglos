ac387e41e9701cb300168239ce7e0947c02db008
dsmv04 <dalton.munoz.v@hotmail.com>
2023-09-14 13:55:45 -0300

    Asignacion 2 dia 4 semana 2

--- desafio5.js
+++ desafio5.js
@@ -7,14 +7,14 @@ es 5. */
 
 function fibonacciArray(n) {
     // [0, 1] son los valores inciales del arreglos para calcular el resto
-    var fibArr = [0, 1];
+    let fibArr = [0, 1];
     while(fibArr.length < n) {
-        var prev = fibArr[fibArr.length-1];
-        var prevprev = fibArr[fibArr.length-2];
+        let prev = fibArr[fibArr.length-1];
+        let prevprev = fibArr[fibArr.length-2];
         fibArr.push(prev + prevprev);
     }
     return fibArr;
 }
    
-var result = fibonacciArray(10);
+let result = fibonacciArray(10);
 console.log(result); // esperamos de vuelta[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
